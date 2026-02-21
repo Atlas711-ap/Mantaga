df2d22da258f07e814461552ef24ccd6a96b7ff3
Anush Prabhu <anush@Anushs-Air.lan>
2026-02-21 11:19:37 +0400

    fix: make Convex pages dynamic to fix build error

--- mission-control-dashboard/src/app/page.tsx
+++ mission-control-dashboard/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useMemo } from "react";
 import { useAgentEventLog, useRecentAgentEvents, useLatestDailyStockSnapshot, useMasterSku } from "../hooks/useConvex";
 
--- mission-control-dashboard/src/app/upload/page.tsx
+++ mission-control-dashboard/src/app/upload/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
